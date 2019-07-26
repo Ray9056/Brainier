a211eb5d2e7c39c8e6525f5cd1c76fba1ecf2ec1
Suraj <surajpatel527@gmail.com>
2019-07-26 16:00:00 -0400

    done needs to deploy to heroku

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Clicky-Game-w-React
+
+[Check out the App](#)
+
+## Step One (Clone the Repository) ##
+
+```
+git clone https://github.com/Jernical/Clicky-Game-w-React.git
+```
+## Step Two (Install Packages!) ##
+
+```
+npm install
+```
+
+## Step Three (Start the app)##
+```
+npm start
+```
---
+++ src/App.js
@@ -0,0 +1,128 @@
+import React, { Component } from "react";
+import MatchCard from "./components/MatchCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import matches from "./matchcards.json";
+import "./App.css";
+
+let correctGuesses = 0;
+let bestScore = 0;
+let clickMessage = "Click on a game cover to gain points! Click on the same one twice and you lose!";
+
+class App extends Component {
+    
+    state = {
+        matches,
+        correctGuesses,
+        bestScore,
+        clickMessage
+    };
+
+    setClicked = id => {
+
+        // Make a copy of the state matches array to work with
+        const matches = this.state.matches;
+
+        // Filter for the clicked match
+        const clickedMatch = matches.filter(match => match.id === id);
+
+        // If the matched image's clicked value is already true, 
+        // do the game over actions
+        if (clickedMatch[0].clicked){
+
+            console.log ("Correct Guesses: " + correctGuesses);
+            console.log ("Best Score: " + bestScore);
+
+            correctGuesses = 0;
+            clickMessage = "Fail! You already clicked on this one."
+
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            this.setState({clickMessage});
+            this.setState({ correctGuesses });
+            this.setState({matches});
+
+        // Otherwise, if clicked = false, and the user hasn't finished
+        } else if (correctGuesses < 11) {
+
+            clickedMatch[0].clicked = true;
+
+            correctGuesses++;
+            
+            clickMessage = "Great Job! You haven't click on that one yet! Keep going!";
+
+            if (correctGuesses > bestScore){
+                bestScore = correctGuesses;
+                this.setState({ bestScore });
+            }
+
+            // Shuffle the array to be rendered in a random order
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            // Set this.state.matches equal to the new matches array
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+        } else {
+
+            // Set its value to true
+            clickedMatch[0].clicked = true;
+
+            // restart the guess counter
+            correctGuesses = 0;
+
+            // Egg on the user to play again
+            clickMessage = "Congratz!!! You got All of them!! Now, let's see if you can do it again!";
+            bestScore = 12;
+            this.setState({ bestScore });
+            
+            for (let i = 0 ; i < matches.length ; i++){
+                matches[i].clicked = false;
+            }
+
+            // Shuffle the array to be rendered in a random order
+            matches.sort(function(a, b){return 0.5 - Math.random()});
+
+            // Set this.state.matches equal to the new matches array
+            this.setState({ matches });
+            this.setState({correctGuesses});
+            this.setState({clickMessage});
+
+        }
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Title>Video Game Clicky Game</Title>
+        
+                <h3 className="scoreSummary">
+                    {this.state.clickMessage}
+                </h3>
+                
+                <h3 className="scoreSummary card-header">
+                    Correct Guesses: {this.state.correctGuesses} 
+                    <br />
+                    Best Score: {this.state.bestScore} 
+                </h3>
+                <div className="container">
+                <div className="row">
+                {this.state.matches.map(match => (
+                    <MatchCard
+                        setClicked={this.setClicked}
+                        id={match.id}
+                        key={match.id}
+                        image={match.image}
+                    />
+                ))}
+                </div>
+                </div>
+
+            </Wrapper>
+        );
+    }
+}
+
+export default App;
